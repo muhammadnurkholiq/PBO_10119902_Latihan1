f2eaefb2ac0cb77b5084d4e7abd78e65877506ef
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:30:46 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan1/src/main/java/com/mycompany/pbo_10119902_latihan1/PBO_10119902_Latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan1;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Muhammad Nurkholiq");
+        System.out.println("ALAMAT\t\t: Jalan Sekeloa Tengah");
+        System.out.println("TEMPAT LAHIR\t: Subang");
+        System.out.println("TANGGAL LAHIR\t: 11 April 2001");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Basket");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Es Teh Manis");
+    }
+}
